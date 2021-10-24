1046f87450a503cfc0abb77bfc651f9248f0de05
aygunbyr <turkoaygun@gmail.com>
2021-10-24 04:26:38 +0300

    Blog first upload
    Blog first upload

---
+++ post/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, HttpResponse, get_object_or_404, HttpResponseRedirect, redirect, Http404
+from .models import Post
+from .forms import PostForm, CommentForm
+from django.contrib import messages
+from django.utils.text import slugify
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+
+
+def post_index(request):
+    post_list = Post.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        post_list = post_list.filter(
+            Q(title__icontains=query) | Q(title__icontains=query.replace('i', 'İ')) | Q(title__icontains=query.replace('ı', 'I')) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+        ).distinct()
+
+    paginator = Paginator(post_list, 5)  # Show 5 posts per page.
+
+    page_number = request.GET.get('sayfa')
+    try:
+        posts = paginator.get_page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        posts = paginator.get_page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        posts = paginator.get_page(paginator.num_pages)
+
+    return render(request, 'post/index.html', {'posts': posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    form = CommentForm(request.POST or None)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        messages.success(request, 'Yorumunuz yönetici tarafından onaylandıktan sonra yayınlanacaktır.', extra_tags='yorum-basarili')
+        return HttpResponseRedirect(post.get_absolute_url())
+
+    context = {
+        'post': post,
+        'form': form,
+    }
+    return render(request, 'post/detail.html', context)
+
+def post_create(request):
+    if not request.user.is_authenticated:
+        return Http404()
+
+    form = PostForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.user = request.user
+        post.save()
+        messages.success(request, 'Başarılı bir şekilde oluşturdunuz.', extra_tags='mesaj-basarili')
+        # extra_tags,  css class'ı belirtir, bu mesajın classları: success, mesaj-basarili
+        return HttpResponseRedirect(post.get_absolute_url())
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'post/form.html', context)
+
+def post_update(request, slug):
+    if not request.user.is_authenticated:
+        return Http404()
+
+    post = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=post)
+    if form.is_valid():
+        if request.user == post.user:
+            form.save()
+            messages.success(request, 'Başarılı bir şekilde düzenlediniz.')
+            return HttpResponseRedirect(post.get_absolute_url())
+        else:
+            messages.warning(request, 'Size ait olmayan bir gönderiyi düzenleyemezsiniz.')
+            return redirect('post:index')
+    context = {
+        'form': form,
+    }
+    return render(request, 'post/form.html', context)
+
+def post_delete(request, slug):
+    if not request.user.is_authenticated:
+        return Http404()
+
+    post = get_object_or_404(Post, slug=slug)
+    if request.user == post.user:
+        post.delete()
+        return redirect('post:index')
+    else:
+        messages.warning(request, 'Size ait olmayan bir gönderiyi silemezsiniz.')
+        return redirect('post:index')
